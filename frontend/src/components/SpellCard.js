3e79fee0cf991df334791e11527db3e8e7be8278
ctbarrel <ctbarrel@gmail.com>
2020-09-29 12:57:29 -0600

    added delete functionality

--- frontend/src/components/SpellCard.js
+++ frontend/src/components/SpellCard.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card'
-// import Button from 'react-bootstrap/Button'
+import Button from 'react-bootstrap/Button'
 
-// const API_URL = process.env.REACT_APP_API_URL
+const API_URL = process.env.REACT_APP_API_URL
 
-export default function SpellCard ({spell}) {
+export default function SpellCard ({spell, refresh}) {
     
+    function handleDelete () {
+        fetch(`${API_URL}spells/${spell._id}`, {
+            method: 'DELETE'
+        })
+        .then(refresh)
+    }
+
     return (
         <Card className='spellcard'>
             <span><span className='spellname'>{spell.name}</span> | {spell.school} {spell.level}</span>
+            <Button className='deletebutton' variant='outline-secondary' onClick={handleDelete}>X</Button>
         </Card>
     )
 }

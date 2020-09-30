0e055c1f7c8f3e862641c6cad2bd27a80d750772
ctbarrel <ctbarrel@gmail.com>
2020-09-30 17:40:01 -0600

    setting up hosting

---
+++ public/precache-manifest.9aa0f1784233f7374c51b7557e8bb38e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "017c239e88f50777c2af9fb22047efa1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd0d146bbebbdac98479",
+    "url": "/static/css/main.f5ad1349.chunk.css"
+  },
+  {
+    "revision": "bc8e854bafbeca4e3ce6",
+    "url": "/static/js/2.61f846b5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.61f846b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd0d146bbebbdac98479",
+    "url": "/static/js/main.b6d38764.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "e3e77af15bdb5cd39babba2c5f39cc36",
+    "url": "/static/media/parchmentbackground.e3e77af1.jpg"
+  }
+]);
